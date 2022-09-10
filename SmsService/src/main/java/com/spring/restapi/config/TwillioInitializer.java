504a4e26dbb8033839b3aaec46573bb2380fcc1d
Ambika-k <ambikakumpatlas44@gmail.com>
2022-09-10 21:33:03 +0530

    updated

--- SmsService/src/main/java/com/spring/restapi/config/TwillioInitializer.java
+++ SmsService/src/main/java/com/spring/restapi/config/TwillioInitializer.java
@@ -6,12 +6,20 @@ import com.twilio.Twilio;
 @Configuration
 public class TwillioInitializer {
 	private final TwillioProperties twillioProperties;
+	public static final String ACCOUNT_SID = System.getenv("TWILIO_ACCOUNT_SID");
+	public static final String AUTH_TOKEN = System.getenv("TWILIO_AUTH_TOKEN");
 
 	public TwillioInitializer(TwillioProperties twillioProperties) {
+		
 		super();
+		System.out.println("twilio intialized with account: "+AUTH_TOKEN);
+		System.out.println("twilio intialized with sid: "+ACCOUNT_SID);
 		this.twillioProperties = twillioProperties;
-		Twilio.init(twillioProperties.getAccountSid(), twillioProperties.getAuthToken());
-		System.out.println("twilio intialized with account: "+twillioProperties.getAccountSid());
+		
+		//Twilio.init(twillioProperties.getAccountSid(), twillioProperties.getAuthToken());
+		Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
+		//System.out.println("twilio intialized with account: "+twillioProperties.getAccountSid());
+		System.out.println("twilio intialized with account: "+AUTH_TOKEN);
 	}
 
 }
